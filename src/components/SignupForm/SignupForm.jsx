2ffb73071d6809e49e013abe762922cb2b9e7711
Marcus Lampley <Marcus@Kathleens-Air.hsd1.ga.comcast.net>
2020-09-18 15:51:07 -0400

    signupform changes

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+import './SignupForm.css'
+import { Form, Input, Button } from 'semantic-ui-react'
 import authService from "../../services/authService";
 
 class SignupForm extends Component {
@@ -8,48 +10,54 @@ class SignupForm extends Component {
     email: "",
     password: "",
     passwordConf: "",
+    avatar: "",
+    phone: "",
+    isSeller: false,
   };
-
   handleChange = (e) => {
     this.props.updateMessage("");
     this.setState({
       [e.target.name]: e.target.value,
     });
   };
-
   handleSubmit = async (e) => {
-    const { history, updateMessage, handleSignupOrLogin } = this.props;
+    const { history, updateMessage, handleSignupOrLogin, } = this.props;
     e.preventDefault();
     try {
       await authService.signup(this.state);
       handleSignupOrLogin();
-      history.push("/");
+        history.push("/");
+        
     } catch (err) {
       updateMessage(err.message);
     }
   };
-
   isFormInvalid() {
     const { name, email, password, passwordConf } = this.state;
     return !(name && email && password === passwordConf);
   }
-
   render() {
-    const { name, email, password, passwordConf } = this.state;
+    const { name, email, password, passwordConf, avatar, phone, isSeller } = this.state;
     return (
-      <div>
-        <h3>Sign Up</h3>
-        <form autoComplete="off" onSubmit={this.handleSubmit}>
+      <Form autoComplete="off" onSubmit={this.handleSubmit}>
+      <h3>Sign Up</h3>
+          <Form.Field>
+          <label htmlFor="name">Name</label>
           <input
+            placeholder="John Doe"
             type="text"
             autoComplete="off"
             id="name"
             value={name}
             name="name"
             onChange={this.handleChange}
-          />
-          <label htmlFor="name">Name</label>
+          />  
+          </Form.Field>
+          <br/>
+          <Form.Field>
+          <label htmlFor="email">Email</label>
           <input
+            placeholder="email@domain.com"
             type="text"
             autoComplete="off"
             id="email"
@@ -57,7 +65,48 @@ class SignupForm extends Component {
             name="email"
             onChange={this.handleChange}
           />
-          <label htmlFor="email">Email</label>
+          </Form.Field>
+          <br/>
+          <Form.Field>
+          <label htmlFor="avatar">Avatar Image</label>
+          <input
+            placeholder="http://www.image.png"
+            type="text"
+            autoComplete="off"
+            id="avatar"
+            value={avatar}
+            name="avatar"
+            onChange={this.handleChange}
+          />
+          </Form.Field>
+          <br/>
+          <Form.Field>
+          <label htmlFor="phone">Phone</label>
+          <input
+            placeholder="(212)867-5309"
+            type="text"
+            autoComplete="off"
+            id="phone"
+            value={phone}
+            name="phone"
+            onChange={this.handleChange}
+          />
+          </Form.Field>
+          <br/>
+          <Form.Field>
+          <label htmlFor="isSeller">Are you a seller?</label>
+          <select
+            id="isSeller"
+            value={isSeller}
+            name="isSeller"
+            onChange={this.handleChange}>
+            <option value="true">Yes</option>
+            <option value="false">No</option>
+          </select>
+          </Form.Field>
+          <br/>
+          <Form.Field>
+          <label htmlFor="password">Password</label>
           <input
             type="password"
             autoComplete="off"
@@ -66,7 +115,10 @@ class SignupForm extends Component {
             name="password"
             onChange={this.handleChange}
           />
-          <label htmlFor="password">Password</label>
+          </Form.Field>
+          <br/>
+          <Form.Field>
+          <label htmlFor="confirm">Confirm Password</label>
           <input
             type="password"
             autoComplete="off"
@@ -75,14 +127,10 @@ class SignupForm extends Component {
             name="passwordConf"
             onChange={this.handleChange}
           />
-          <label htmlFor="confirm">Confirm Password</label>
-          <button disabled={this.isFormInvalid()}>Sign Up</button>
-          &nbsp;&nbsp;
-          <Link to="/">Cancel</Link>
-        </form>
-      </div>
+          </Form.Field>
+          <Button disabled={this.isFormInvalid()}>Sign Up</Button>
+        </Form>
     );
   }
 }
-
 export default SignupForm;
