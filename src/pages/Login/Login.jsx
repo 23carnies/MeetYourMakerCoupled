063d3ed7de7fae3172058fd7297301e5565dcc54
RobBloxham <acidwash52@gmail.com>
2020-09-18 09:21:09 -0700

    styling login page and adding semantic ui

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import authService from "../../services/authService";
+import { Form, Image, Container, Button, Card } from 'semantic-ui-react'
+import './login.css'
 
 class Login extends Component {
   state = {
@@ -30,33 +32,43 @@ class Login extends Component {
   render() {
     const {email, pw} = this.state
     return (
-      <main className="Login">
-        <h3>Log In</h3>
-        <form autoComplete="off" onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            autoComplete="off"
-            id="email"
-            value={email}
-            name="email"
-            onChange={this.handleChange}
-          />
-          <label htmlFor="email">Email</label>
-          <input
-            type="password"
-            autoComplete="off"
-            id="password"
-            value={pw}
-            name="pw"
-            onChange={this.handleChange}
-          />
-          <label htmlFor="password">Password</label>
-          <button className="btn green">Log In</button>&nbsp;&nbsp;&nbsp;
-          <Link className="btn red" to="/">
-            Cancel
-          </Link>
-        </form>
-      </main>
+      <Container id="mess">
+        <Card fluid id="lc">
+          <Image id="image"src="https://www.bates.edu/wordpress/files/2016/07/texture-16.jpg" /> <br/>
+          <Card.Header id="hdf">Log In</Card.Header><br/>
+          <Form autoComplete="off" onSubmit={this.handleSubmit}>
+          <br/> 
+            <Form.Field>
+            <input
+              type="text"
+              autoComplete="off"
+              id="email"
+              value={email}
+              name="email"
+              onChange={this.handleChange}
+            />
+            <label htmlFor="email">Email</label>
+            </Form.Field>
+            <br/>
+            <Form.Field>
+            <input
+              type="password"
+              autoComplete="off"
+              id="password"
+              value={pw}
+              name="pw"
+              onChange={this.handleChange}
+            />
+            <label htmlFor="password">Password</label>
+            </Form.Field> <br/> 
+            <Button>Log In</Button>&nbsp;&nbsp;&nbsp;
+            <Link className="btn red" to="/signup">
+              <Button>Signup</Button>
+            </Link>
+            <br/> <br/> <br/> <br/> 
+          </Form>
+        </Card>
+      </Container>
     );
   }
 }
