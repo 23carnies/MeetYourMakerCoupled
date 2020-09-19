d2891736f1fa4e82b9fce0fba1d067c4b6894a85
23carnies <46233990+23carnies@users.noreply.github.com>
2020-09-19 17:30:37 -0400

    Basic product card component

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -5,9 +5,11 @@ import { Card } from 'semantic-ui-react'
 
 const ProductCard = (props) => (
     <Card
+      href='#'
       header={product.name}
       image={product.image}
       meta={product.maker}
+      meta={store.storeLocation}
       description={product.price}
       description={product.category}
       extra={product.countInStock}
