882e5f0245028c3a94b1a957d3fbb67f0ae4d714
RobBloxham <acidwash52@gmail.com>
2020-09-23 18:15:13 -0700

    updating nav bar for conditions

--- src/components/NavBar/NavBarOld.jsx
+++ src/components/NavBar/NavBarOld.jsx
@@ -20,7 +20,6 @@ export default class NavBar extends Component {
         </Menu.Item>
 
         <Menu.Item
-          position='right'
           name='sign-up'
           active={activeItem === 'testimonials'}
           onClick={this.handleItemClick}
@@ -29,6 +28,7 @@ export default class NavBar extends Component {
         </Menu.Item>
 
         <Menu.Item
+          position='right'
           name='log-in'
           active={activeItem === 'sign-in'}
           onClick={this.handleItemClick}
@@ -40,14 +40,6 @@ export default class NavBar extends Component {
         //These links are for Users that are not Sellers------------
         user.isSeller===false ?
         <>
-      <Menu.Item>
-        <img src='/logo.png' />
-      </Menu.Item>
-      <Menu.Item>
-        Welcome {user.name}!
-      </Menu.Item>
-
-
         {/* Nav link to Seller */}
         <Menu.Item
         name='sellers'
@@ -64,6 +56,12 @@ export default class NavBar extends Component {
       >
         <Link to='/calendar' >Events</Link>        
       </Menu.Item>
+        <Menu.Item>
+          <img src='/logo.png' />
+        </Menu.Item>
+        <Menu.Item>
+          What?
+        </Menu.Item>
         <Menu.Item
           position='right'
           name='log-out'
@@ -73,18 +71,18 @@ export default class NavBar extends Component {
         <Link to='' onClick={handleLogout}>LOG OUT</Link>
         </Menu.Item>
         </>
-        : user.store.length === 0 ?
-        // These Links Are for Sellers Without a store -------
+        :
+        // These Links Are for Sellers-------------------
         <>
         <Menu.Item>
         <img src='/logo.png' />
       </Menu.Item>
       <Menu.Item>
-        Welcome {user.name}!
-      </Menu.Item>
-
-    {/* Nav link to SellerSetup form */}
-    <Menu.Item
+        inbox
+      </Menu.Item> 
+     
+      {/* Nav link to SellerSetup form */}
+      <Menu.Item
         name='setup-store'
         active={activeItem === 'setup-store'}
         onClick={this.handleItemClick}
@@ -92,7 +90,6 @@ export default class NavBar extends Component {
         <Link to='/setup-store' >Setup Store</Link>
         
       </Menu.Item>
-     
             {/* Nav link to Seller */}
             <Menu.Item
         name='sellers'
@@ -109,8 +106,6 @@ export default class NavBar extends Component {
       >
         <Link to='/calendar' >Events</Link>        
       </Menu.Item>
-
-
       
           {/* Log out */}
           <Menu.Item
@@ -122,52 +117,6 @@ export default class NavBar extends Component {
       <Link to='' onClick={handleLogout}>LOG OUT</Link>
       </Menu.Item>
       </>
-      :
-      <>
-        <Menu.Item>
-        <img src='/logo.png' />
-        </Menu.Item>
-        <Menu.Item>
-        Welcome back {user.name}!
-      </Menu.Item>
-      {/* Nav link to Inbox  */}
-      <Menu.Item
-      name='inbox'
-      active={activeItem === 'inbox'}
-      onClick={this.handleItemClick}
-      >        
-        Inbox
-      </Menu.Item> 
-          
-          {/* Nav link to Seller */}
-          <Menu.Item
-      name='sellers'
-      active={activeItem === 'sellers'}
-      onClick={this.handleItemClick}
-    >
-      <Link to='/sellers' >Makers</Link>
-      </Menu.Item>
-            {/* Nav link to Calendar form */}
-    <Menu.Item
-      name='calendar'
-      active={activeItem === 'calendar'}
-      onClick={this.handleItemClick}
-    >
-      <Link to='/calendar' >Events</Link>        
-    </Menu.Item>
-
-          {/* Log out */}
-          <Menu.Item
-        position='right'
-        name='log-out'
-        active={activeItem === 'log-out'}
-        onClick={this.handleItemClick}
-      >
-      <Link to='' onClick={handleLogout}>LOG OUT</Link>
-      </Menu.Item>
-  
-    </>
-
      }
      </Menu>  
      )
